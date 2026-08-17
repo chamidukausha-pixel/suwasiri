710c05269f52f2a46a07380ab008046a0681b94c
Chamidu <chamidukausha@gmail.com>
2026-08-17 21:12:41 +1000

    second commit

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -16,6 +16,7 @@ abstract final class AppColors {
   static const Color vaultGreen = Color(0xFF004D40);
   static const Color emeraldSoft = Color(0xFFECFDF5);
   static const Color tipTeal = Color(0xFF065F46);
+  static const Color videoBrown = Color(0xFF5D4037);
   static const Color videoPurple = Color(0xFF7C3AED);
   static const Color videoPurpleSoft = Color(0xFFF5F3FF);
   static const Color labBeige = Color(0xFFF5E6D3);
